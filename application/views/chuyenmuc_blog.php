e6214cd4aaa65635df35e17c24aa8c2216687fad
Hoàng Tuấn Khanh <agola3bie@gmail.com>
2023-06-27 15:12:03 +0700

    lan 4

--- application/views/chuyenmuc_blog.php
+++ application/views/chuyenmuc_blog.php
@@ -18,32 +18,21 @@
         <div class="train_content">
             <div class="top_blog">
                 <div class="top_left">
-                    <div class="list_blog_top">
-                        <div class="left_blog">
-                            <div class="blog_top">
-                                <?php foreach ($blog as $key => $val) {
-                                    if ($key == 0) { ?>
-                                <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
-                                    <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
-                                    <p class="title_blog_top"><?= $val['title'] ?></p>
-                                    <div class="fl_date">
-                                        <p class="date_post"><?= date('d-m-Y', $val['created_at']) ?></p>
-                                    </div>
-                                    <div class="sapo_blog_top"><?= $val['sapo'] ?></div>
-                                </a>
-                                <?php }
-                                } ?>
-                            </div>
-                        </div>
-                        <div class="right_blog">
+                    <div class="left_blog">
+                        <div class="blog_top">
                             <?php foreach ($blog as $key => $val) {
-                                if ($key < 4 && $key > 0) { ?>
-                            <div class="this_train_right">
-                                <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
-                                    <p class="title_blog"><?= $val['title'] ?></p>
-                                    <div class="des_blog"><?= $val['sapo'] ?></div>
-                                </a>
-                            </div>
+                                if ($key == 0) { ?>
+                            <a class="linl_all_detail" title="<?= $val['title'] ?>" href="/<?= $val['alias'] ?>/">
+                                <div class="blog_top_content blog_top_left">
+                                    <img src="/<?= $val['image'] ?>" alt="<?= $val['title'] ?>">
+                                </div>
+                                <div class="blog_top_content blog_top_right">
+                                    <p class="title_blog_top"><?= $val['title'] ?></p>
+                                    <div class="sapo_blog"><?= $val['sapo'] ?>
+
+                                    </div>
+                                </div>
+                            </a>
                             <?php }
                             } ?>
                         </div>
